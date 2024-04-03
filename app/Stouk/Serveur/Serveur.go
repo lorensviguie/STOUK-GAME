661941af24b1bf7c05999bfe47f7338cfc157671
Gwuill <guill.branche@gmail.com>
2024-04-03 09:01:25 +0200

    fix tailwind

--- app/Stouk/Serveur/Serveur.go
+++ app/Stouk/Serveur/Serveur.go
@@ -15,11 +15,17 @@ type HtmlData struct {
 
 func ServeurInit() {
 	fmt.Println("Serveur is running on localhost:8000")
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
+
 
 	http.HandleFunc("/", HomeHandler)
 	http.HandleFunc("/login", LoginHandler)
 	http.HandleFunc("/register", RegisterHandler)
 	http.HandleFunc("/dice", DiceHandler)
-	http.ListenAndServe(":8000", nil)
+	
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
 
 }
