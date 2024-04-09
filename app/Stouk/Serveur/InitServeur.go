e01da442c603d3c4b4eaad41dc48d7614034a388
Gwuill <guill.branche@gmail.com>
2024-04-09 14:31:13 +0200

    fix

--- app/Stouk/Serveur/InitServeur.go
+++ app/Stouk/Serveur/InitServeur.go
@@ -5,14 +5,6 @@ import (
 	"net/http"
 )
 
-type HtmlData struct {
-	IsLoggedIn  bool
-	IsModerator bool
-	IsAdmin     bool
-	PageName    string
-	Fragments   map[string]string
-}
-
 func ServeurInit() {
 	fmt.Println("Serveur is running on localhost:8000")
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
